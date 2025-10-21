db98840b4a73c1645e6cd1b33918bdcf18fc2e88
geancavalcante <geanfeitosacavalcanteof@gmail.com>
2025-10-21 20:11:34 -0300

    adicionando apps

--- attend_finence/settings.py
+++ attend_finence/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'solicitacoes',
+    'usuarios',
+    'dashborads',
 ]
 
 MIDDLEWARE = [
