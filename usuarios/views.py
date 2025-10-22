80b267773dc27d1901a4596b465878d341d1399c
geancavalcante <geanfeitosacavalcanteof@gmail.com>
2025-10-22 11:55:53 -0300

    views_recepção_data

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import View
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+class LoginUsuarios(View):
+
+    def get(request):
+        return render(request, "login.html")
+    
+    def post(request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            redirect("/")
