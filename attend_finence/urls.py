a38722def59a67ebc2c754ab943a9886938c9ea5
Analista Operacional <guilhermetorres1202@gmail.com>
2025-11-03 15:01:22 -0300

    correção area de gerenciar membros e imagens de login

--- attend_finence/urls.py
+++ attend_finence/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from solicitacoes import views as solicitacoes_views
 
 urlpatterns = [
@@ -10,3 +12,15 @@ urlpatterns = [
     path("servicos/", include("servicos.urls")),
     path("dashboards/", include("dashborads.urls")), # Mantido para dashboards
 ]
+
+# Servir arquivos estáticos
+# Em desenvolvimento: Django serve automaticamente
+# Em produção: serve do STATIC_ROOT (após collectstatic) ou configure Nginx/Apache
+if settings.DEBUG:
+    # Em modo debug, usa staticfiles_urlpatterns que serve de STATICFILES_DIRS
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+else:
+    # Em produção, serve do STATIC_ROOT (necessita rodar collectstatic antes)
+    if hasattr(settings, 'STATIC_ROOT') and settings.STATIC_ROOT:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
